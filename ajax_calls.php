e5da9ffd6a467575c7b55bd8d6c1982e77b61d05
adhikansh1999 <singhadhikansh113@gmail.com>
2018-12-26 17:45:49 +0530

    ilu added

--- ajax_calls.php
+++ ajax_calls.php
@@ -1,7 +1,11 @@
 <?php
 	require_once('connection.php');
 
+/*
+problems in sponsor_title database: the numbers are all wrong
+Also titleId in spons_list corresponds to the relaevant title not order_no
 
+*/
   class Product {
     
     public $sno;
@@ -24,24 +28,12 @@
       $this->order_no = $order_no;
     }
 
-    public static function all() {
-      $list = [];
-      $db = Db::getInstance();
-      $req = $db->query('SELECT * FROM spons_list');
-
-      foreach($req->fetchAll() as $product) {
-        $list[] = new product($product['sno'], $product['sub_title'], $product['name'], $product['link_img'], $product['link_web'], $product['title_id'],$product['year'], $product['order_no']);
-      }
-
-      return $list;
-    }
-
     public static function find($year) {
       $db = Db::getInstance();
 
       $year = intval($year);
 
-      $req = $db->prepare('SELECT * FROM spons_list WHERE year = :year');
+      $req = $db->prepare('SELECT * FROM spons_list WHERE year = :year ORDER BY title_id');
       $req->execute(array('year' => $year));
       
 
@@ -53,18 +45,68 @@
     }
   }
 
+
+  class titles {
+    
+    public $id;
+    public $title;
+    public $number;
+    public $sizeTitle;
+    public $year;
+    public $order_no;
+
+    public function __construct($id, $title, $number, $sizeTitle, $year, $order_no) {
+      $this->id      = $id;
+      $this->title  = $title;
+      $this->sizeTitle=$sizeTitle;
+      $this->year=$year;
+      $this->order_no=$order_no;
+      $this->number=$number;
+    }
+
+    public static function find($year) {
+      $db = Db::getInstance();
+
+      $year = intval($year);
+
+      $req = $db->prepare('SELECT * FROM spons_title WHERE year = :year');
+      $req->execute(array('year' => $year));
+      
+
+      foreach($req->fetchAll() as $titles) {
+        $list[] = new titles($titles['id'], $titles['title'], $titles['number'], $titles['sizeTitle'], $titles['year'], $titles['order_no']);
+      }
+
+      return $list;
+    }
+  }
+
+
+
 ?>
 
 <?php
-	echo "the year is  <h1>". $_POST['year']."</h1>";
+	echo "<h1 style = 'font-size: 7vw;'>". $_POST['year']."</h1>";
 
 
   $test = Product::find($_POST['year']);
+  $heading = titles::find($_POST['year']);
+  $it = 0;
 
-  echo '<br><br><br>'.sizeof($test);
-
+  // // foreach ($heading as $hd) 
+  // // {
+  // 	echo '<br><h1>'.$heading[0]->title.'</h1>';
+  // 	echo '<br><h1>'.$heading[1]->title.'</h1>';
+  // // }
+  $temp = -1;
   foreach ($test as $data) 
   {
+  		if($data->title_id != $temp)
+  		{
+  			echo '<br><h1>'.$heading[$it]->title.'</h1>';
+  			$it = $it + 1;
+  		}
+  		 
 		echo 
 		'<div class="col-md-4 col-sm-6 portfolio-item" style="margin:auto;">
 		<a href="'.$data->link_web.'">
@@ -76,6 +118,7 @@
 		 	echo '<p class="text-muted">'.$data->sub_title.'</p>';
 		}
 		echo '</div>';
+		$temp = $data->title_id;
   }
   // if the link is empty put sf website in place of the original using the $_GLOBAL[''] variable
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!-- the bluish color of the main website is 	#170849 -->
-<!-- <img src="sponsors/sf2016/letspool.png" alt="eff yeah"> -->
+<!-- cowboy theme color is #FAA66A -->
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,42 +21,49 @@
 </head>
 
 <body id="page-top">		
-
+	<!-- blue color is background-color: #170849 -->
     <!-- Navigation Bar  -->
     <!-- <nav class="mb-4 navbar navbar-expand-lg navbar-dark purple lighten-1"> -->
-    <nav class="mb-4 navbar navbar-expand-lg navbar-dark lighten-1" id= "navbar" style="background-color: #170849;">
-                        <a class="navbar-brand js-scroll-trigger" href="https://springfest.in/sponsors/#page-top"><img src="./sponsors_files/sf_logo.png" width="45vh"></a>
+    <nav class="mb-0 navbar navbar-expand-lg navbar-dark lighten-1" id= "navbar" style="background-color: #A9B7c0;
+    padding-bottom: 0px;">
+                        <a class="navbar-brand js-scroll-trigger" href="https://springfest.in/sponsors/#page-top"><img src="./sponsors_files/sf_logo.png" width="46vh"></a>
 
-                <a class="navbar-brand" href="https://springfest.in/sponsors/#page-top">Sponsors: Spring Fest</a>
+                <a class="navbar-brand" href="https://springfest.in/sponsors/#page-top"><h6>Sponsors: Spring Fest</h6></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-5" aria-controls="navbarSupportedContent-5" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent-5">
                     <ul class="navbar-nav mr-auto">
+                    	<li class="nav-item">
+			              <a class="nav-link js-scroll-trigger" onclick="changeyear(2019);">
+			              <i class="fa fa-calendar"></i>2019</a>
+			            </li>
                         <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" onclick="changeyear(2018);">2018</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" onclick="changeyear(2017);">2017</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" onclick="changeyear(2016);">2016</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" onclick="changeyear(2015);">2015</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" onclick="changeyear(2014);">2014</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" onclick="changeyear(2013);">2013</a>
-            </li>
+			              <a class="nav-link js-scroll-trigger" onclick="changeyear(2018);"><i class="fa fa-calendar"></i>2018</a>
+			            </li>
+			            <li class="nav-item">
+			              <a class="nav-link" onclick="changeyear(2017);"><i class="fa fa-calendar"></i>2017</a>
+			            </li>
+			            <li class="nav-item">
+			              <a class="nav-link" onclick="changeyear(2016);"><i class="fa fa-calendar"></i>2016</a>
+			            </li>
+			            <li class="nav-item">
+			              <a class="nav-link" onclick="changeyear(2015);"><i class="fa fa-calendar"></i>2015</a>
+			            </li>
+			            <li class="nav-item">
+			              <a class="nav-link" onclick="changeyear(2014);"><i class="fa fa-calendar"></i>2014</a>
+			            </li>
+			            <li class="nav-item">
+			              <a class="nav-link" onclick="changeyear(2013);"><i class="fa fa-calendar"></i>2013</a>
+			            </li>
                     </ul>
-                    <ul class="navbar-nav ml-auto nav-flex-icons">
+                    <!-- <ul class="navbar-nav ml-auto nav-flex-icons">
                         <li class="nav-item">
-                            <a class="nav-link waves-effect waves-light">1 <i class="fa fa-envelope"></i></a>
+                            <a class="nav-link waves-effect waves-light">
+                            	<i class="fa fa-calendar"></i>
+                            </a>
                         </li>
-                    </ul>
+                    </ul> -->
                 </div>
             </nav>
      
@@ -68,13 +75,15 @@
     <header class="masthead">
       <div class="container">
                     <div class="row block">
-          <img src="./sponsors_files/logo.png" style="width: 75vw; padding-bottom: 1.5em	;">
-          <img src="./sponsors_files/logo2.png" style="width: 15vw;">
+          <img src="./sponsors_files/logo2.png" style="width: 75vw; padding-bottom: 1.5em	;">
+          <!-- <img src="./sponsors_files/logo2.png" style="width: 15vw;"> -->
 
           </div>
             <div class="row team-members">
               <div class="col-xs-12">
+              	<h2>
                  Sponsorship and Marketing Heads:
+             	</h2>
               </div>
             </div>
             <div class="row no-flex">
@@ -191,38 +200,7 @@
     <footer class="bg-light">
       <div class="container">
         <div class="row">
-          <div class="col-md-3">
-          </div>
-          <div class="col-md-6">
-            <ul class="list-inline social-buttons">
-              <li class="list-inline-item">
-                <a href="https://www.instagram.com/iitkgp.springfest/" target="_blank">
-                  <i class="fa fa-instagram"></i>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="https://twitter.com/springfest_kgp" target="_blank">
-                  <i class="fa fa-twitter"></i>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="https://www.facebook.com/springfest.iitkgp/" target="_blank">
-                  <i class="fa fa-facebook"></i>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="https://www.youtube.com/channel/UCTwvia6nV4rReW6_jSsKANA" target="_blank">
-                  <i class="fa fa-youtube"></i>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="https://in.linkedin.com/company/spring-fest" target="_blank">
-                  <i class="fa fa-linkedin"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+          
       </div>
          <div class="page-content links">
 -        <div class="social">
@@ -234,6 +212,7 @@
 -          <span><a class="social_icons" target="_blank" href="https://www.linkedin.com/company/spring-fest/"><img src="./img/linkedin.svg" width="40" class="social_img"/></a></span>
 -        </div>
 -      </div>
+		</div>
     </footer>
 
 	<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
