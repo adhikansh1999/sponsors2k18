9715528ff026fcf91952b440bf07556082c7dff1
adhikansh1999 <34066096+adhikansh1999@users.noreply.github.com>
2019-01-31 22:09:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<!-- the bluish color of the main website is 	#170849 -->
-<!-- <img src="sponsors/sf2016/letspool.png" alt="eff yeah"> -->
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,6 +9,8 @@
 	<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css'>
 	<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css'>
 	<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.4.1/css/mdb.min.css'>
+  
+  <link href="./sponsors_files/sf_logo.png" rel="icon" type="image/x-icon">
   	
 
 	<!-- Custom styles for this template -->
@@ -21,42 +21,47 @@
 </head>
 
 <body id="page-top">		
+    <nav class="mb-0 navbar navbar-expand-lg navbar-dark lighten-1" id= "navbar" style="background-color: #7e6336;
+    padding-bottom: 0px;">
+                        <a class="navbar-brand js-scroll-trigger" href="https://springfest.in/sponsors/#page-top"><img src="./sponsors_files/sf_logo.png" width="46vh"></a>
 
-    <!-- Navigation Bar  -->
-    <!-- <nav class="mb-4 navbar navbar-expand-lg navbar-dark purple lighten-1"> -->
-    <nav class="mb-4 navbar navbar-expand-lg navbar-dark lighten-1" id= "navbar" style="background-color: #170849;">
-                        <a class="navbar-brand js-scroll-trigger" href="https://springfest.in/sponsors/#page-top"><img src="./sponsors_files/sf_logo.png" width="45vh"></a>
-
-                <a class="navbar-brand" href="https://springfest.in/sponsors/#page-top">Sponsors: Spring Fest</a>
+                <a class="navbar-brand" href="https://springfest.in/sponsors/#page-top"><h6>Sponsors: Spring Fest</h6></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-5" aria-controls="navbarSupportedContent-5" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent-5">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" onclick="changeyear(2018);">2018</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" onclick="changeyear(2017);">2017</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" onclick="changeyear(2016);">2016</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" onclick="changeyear(2015);">2015</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" onclick="changeyear(2014);">2014</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" onclick="changeyear(2013);">2013</a>
-            </li>
-                    </ul>
+                    <!-- <ul class="navbar-nav mr-auto"> -->
                     <ul class="navbar-nav ml-auto nav-flex-icons">
+                    	<li class="nav-item">
+			              <a class="nav-link js-scroll-trigger" onclick="changeyear(2019);">
+			              <i class="fa fa-calendar"></i>2019</a>
+			            </li>
                         <li class="nav-item">
-                            <a class="nav-link waves-effect waves-light">1 <i class="fa fa-envelope"></i></a>
-                        </li>
+			              <a class="nav-link js-scroll-trigger" onclick="changeyear(2018);"><i class="fa fa-calendar"></i>2018</a>
+			            </li>
+			            <li class="nav-item">
+			              <a class="nav-link" onclick="changeyear(2017);"><i class="fa fa-calendar"></i>2017</a>
+			            </li>
+			            <li class="nav-item">
+			              <a class="nav-link" onclick="changeyear(2016);"><i class="fa fa-calendar"></i>2016</a>
+			            </li>
+			            <li class="nav-item">
+			              <a class="nav-link" onclick="changeyear(2015);"><i class="fa fa-calendar"></i>2015</a>
+			            </li>
+			            <li class="nav-item">
+			              <a class="nav-link" onclick="changeyear(2014);"><i class="fa fa-calendar"></i>2014</a>
+			            </li>
+			            <li class="nav-item">
+			              <a class="nav-link" onclick="changeyear(2013);"><i class="fa fa-calendar"></i>2013</a>
+			            </li>
                     </ul>
+                    <!-- <ul class="navbar-nav ml-auto nav-flex-icons">
+                        <li class="nav-item">
+                            <a class="nav-link waves-effect waves-light">
+                            	<i class="fa fa-calendar"></i>
+                            </a>
+                        </li>
+                    </ul> -->
                 </div>
             </nav>
      
@@ -68,21 +73,23 @@
     <header class="masthead">
       <div class="container">
                     <div class="row block">
-          <img src="./sponsors_files/logo.png" style="width: 75vw; padding-bottom: 1.5em	;">
-          <img src="./sponsors_files/logo2.png" style="width: 15vw;">
+          <img src="./sponsors_files/logo2.png" style="width: 90vw;height: auto; padding-bottom: 1.5em; margin-left: -10%;">
 
           </div>
             <div class="row team-members">
               <div class="col-xs-12">
+              	<h2>
                  Sponsorship and Marketing Heads:
+             	</h2>
               </div>
             </div>
             <div class="row no-flex">
               <div class="col-sm-4 col-6">
                 <div class="team-member">
+                  <img src="sponsors_files/img/Shreyash Gupta.jpg" style="padding-bottom: 0px;">
                   <h4>Shreyash <br> Gupta</h4>
                   <p class="text-muted" style="margin-bottom: 0"><a href="mailto:marketing@springfest.in"><span class="__cf_email__" data-cfemail="492a2128203d28273028092426262d2067263b2e"> marketing@springfest.in</span></a></p>
-                  <p class="text-muted" style="margin-bottom: 0"><a href="tel:+918879443797">+91-7355538163</a></p> 
+                  <p class="text-muted" style="margin-bottom: 0"><a href="tel:+917355538163">+91-7355538163</a></p> 
                   <ul class="list-inline social-buttons">
                     <li class="list-inline-item">
                       <a href="https://www.facebook.com/shreyash.gupta.3557">
@@ -99,9 +106,10 @@
               </div>
               <div class="col-sm-4 col-6">
                 <div class="team-member">
+                  <img src="sponsors_files/img/Hritik Khetan.jpg" style="padding-bottom: 0px;">
                   <h4>Hritik <br> Khetan</h4>
                   <p class="text-muted" style="margin-bottom: 0"><a href="mailto:marketing@springfest.in"><span class="__cf_email__" data-cfemail="c8a0a9babba088a5a7a7aca1e6a7baaf"> marketing@springfest.in</span></a></p>
-                  <p class="text-muted" style="margin-bottom: 0"><a href="tel:+918879443797">+91-8094935179</a></p>
+                  <p class="text-muted" style="margin-bottom: 0"><a href="tel:+918094935179">+918094935179</a></p>
                   <ul class="list-inline social-buttons">
                     <li class="list-inline-item">
                       <a href="https://www.facebook.com/hritik.khetan.7">
@@ -118,9 +126,11 @@
               </div>
               <div class="col-sm-4 col-6">
                 <div class="team-member">
+
+                  <img src="sponsors_files/img/Shreyansh Shah.jpg" style="padding-bottom: 0px;">
                   <h4>Shreyansh<br> Shah</h4>
                   <p class="text-muted" style="margin-bottom: 0"><a href="mailto:marketing@springfest.in"><span class="__cf_email__" data-cfemail="a5d6c4cbcec0d1e5c8cacac1cc8bcad7c2"> marketing@springfest.in</span></a></p>
-                  <p class="text-muted" style="margin-bottom: 0"><a href="tel:+918879443797">+91-9674624120</a></p>
+                  <p class="text-muted" style="margin-bottom: 0"><a href="tel:+919674624120">+91-9674624120</a></p>
                   <ul class="list-inline social-buttons">
                     <li class="list-inline-item">
                       <a href="https://www.facebook.com/shreyanshshah22">
@@ -137,9 +147,11 @@
               </div>
 			  <div class="col-sm-4 offset-sm-2 col-6">
                 <div class="team-member">
+
+                  <img src="sponsors_files/img/Nishi Bajaj.jpg" style="padding-bottom: 0px;">
                   <h4>Nishi <br> Bajaj</h4>
                   <p class="text-muted" style="margin-bottom: 0"><a href="mailto:marketing@springfest.in"><span class="__cf_email__" data-cfemail="a5d6c4cbcec0d1e5c8cacac1cc8bcad7c2"> marketing@springfest.in</span></a></p>
-                  <p class="text-muted" style="margin-bottom: 0"><a href="tel:+918879443797">+91-7477738111</a></p>
+                  <p class="text-muted" style="margin-bottom: 0"><a href="tel:+917477738111">+91-7477738111</a></p>
                   <ul class="list-inline social-buttons">
                     <li class="list-inline-item">
                       <a href="https://www.facebook.com/nishi.bajaj.9">
@@ -157,9 +169,11 @@
 
            <div class="col-sm-4 col-6 offset-3 offset-sm-0">
                 <div class="team-member">
+
+                  <img src="sponsors_files/img/Vitthal Agarwal.jpg" style="padding-bottom: 0px;">
                   <h4>Vitthal <br> Agarwal</h4>
                   <p class="text-muted" style="margin-bottom: 0"><a href="mailto:marketing@springfest.in"><span class="__cf_email__" data-cfemail="a5d6c4cbcec0d1e5c8cacac1cc8bcad7c2"> marketing@springfest.in</span></a></p>
-                  <p class="text-muted" style="margin-bottom: 0"><a href="tel:+918879443797">+91-7427933888</a></p>
+                  <p class="text-muted" style="margin-bottom: 0"><a href="tel:+917427933888">+91-7427933888</a></p>
                   <ul class="list-inline social-buttons">
                     <li class="list-inline-item">
                       <a href="https://www.facebook.com/vitthal.agarwal">
@@ -191,56 +205,27 @@
     <footer class="bg-light">
       <div class="container">
         <div class="row">
-          <div class="col-md-3">
-          </div>
-          <div class="col-md-6">
-            <ul class="list-inline social-buttons">
-              <li class="list-inline-item">
-                <a href="https://www.instagram.com/iitkgp.springfest/" target="_blank">
-                  <i class="fa fa-instagram"></i>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="https://twitter.com/springfest_kgp" target="_blank">
-                  <i class="fa fa-twitter"></i>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="https://www.facebook.com/springfest.iitkgp/" target="_blank">
-                  <i class="fa fa-facebook"></i>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="https://www.youtube.com/channel/UCTwvia6nV4rReW6_jSsKANA" target="_blank">
-                  <i class="fa fa-youtube"></i>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="https://in.linkedin.com/company/spring-fest" target="_blank">
-                  <i class="fa fa-linkedin"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+          
       </div>
-         <div class="page-content links">
+         <div class="page-content links" style="color: #f8f9fa">
 -        <div class="social">
--          <span><a class="social_icons" target="_blank" href="http://www.facebook.com/springfest.iitkgp"><img src="./img/facebook.svg" width="40" class="social_img"/></a></span>
--          <span><a class="social_icons" target="_blank" href="https://twitter.com/springfest_kgp"><img src="./img/twitter.svg" width="40" class="social_img"/></a></span>
--          <span><a class="social_icons" target="_blank" href="https://www.instagram.com/iitkgp.springfest/"><img src="./img/instagram.svg" width="40" class="social_img"/></a></span>
--          <span><a class="social_icons" target="_blank" href="https://www.youtube.com/user/SpringFestForever"><img src="./img/youtube.svg" width="40" class="social_img"/></a></span>
--          <span><a class="social_icons" target="_blank" href="https://plus.google.com/+SpringfestIn/"><img src="./img/google-plus.svg" width="40" class="social_img"/></a></span>
--          <span><a class="social_icons" target="_blank" href="https://www.linkedin.com/company/spring-fest/"><img src="./img/linkedin.svg" width="40" class="social_img"/></a></span>
+-          <span><a class="social_icons" target="_blank" href="http://www.facebook.com/springfest.iitkgp"><img src="./sponsors_files/img/facebook.svg" width="40" class="social_img"/></a></span>
+-          <span><a class="social_icons" target="_blank" href="https://twitter.com/springfest_kgp"><img src="./sponsors_files/img/twitter.svg" width="40" class="social_img"/></a></span>
+-          <span><a class="social_icons" target="_blank" href="https://www.instagram.com/iitkgp.springfest/"><img src="./sponsors_files/img/instagram.svg" width="40" class="social_img"/></a></span>
+-          <span><a class="social_icons" target="_blank" href="https://www.youtube.com/user/SpringFestForever"><img src="./sponsors_files/img/youtube.svg" width="40" class="social_img"/></a></span>
+-          <span><a class="social_icons" target="_blank" href="https://plus.google.com/+SpringfestIn/"><img src="./sponsors_files/img/google-plus.svg" width="40" class="social_img"/></a></span>
+-          <span><a class="social_icons" target="_blank" href="https://www.linkedin.com/company/spring-fest/"><img src="./sponsors_files/img/linkedin.svg" width="40" class="social_img"/></a></span>
 -        </div>
 -      </div>
+		</div>
     </footer>
+
 
 	<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 	<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js'></script>
 	<script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js'></script>
 	<script src='https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.4.1/js/mdb.min.js'></script>
-
+  <script src="script.js"></script>
 	<script>
 			function changeyear(year){
 			$.ajax({
@@ -283,7 +268,7 @@
 			  });
 
 			}
-			changeyearinit(2018);
+			changeyearinit(2019);
 	</script>
 
 
